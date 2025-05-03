b7dff99db895b3a576bf7b38dc481fd2a9addb43
HaiPH <Haiph0208@gmail.com>
2025-05-03 09:00:12 +0700

    Initial Commit

---
+++ ChillMovie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChillMovie
+//
+//  Created by Hai Pham on 3/5/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
